675a78904a3b9057937cf4ad7505483b500279f8
kujbusadamattila120 <adamka20080920@gmail.com>
2026-05-21 14:16:00 +0200

    kezdeti storyNodes adatszerkezet letrehozasa az elso fejezetekkel

---
+++ app.js
@@ -0,0 +1,24 @@
+// fejezetek
+let storyNodes = {
+    1: {
+        text: "Gyalogszerrel indulsz utnak. A Tuzhegy labanal egy sotet barlangbejaratot talsz. Belepsz a jaratba. Elotted a folyosó egyenesen halad, de par meter utan egy elagazashoz ersz.",
+        gomb1_szoveg: "Fordulj Nyugat fele",
+        gomb1_hova: 2,
+        gomb2_szoveg: "Fordulj Kelet fele",
+        gomb2_hova: 3
+    },
+    2: {
+        text: "A nyugati jarat egyre szukebb lesz, vegul egy nehez tolgyfa ajtohoz ersz. Halk horkolast hallasz odabentrol.",
+        gomb1_szoveg: "Berugod az ajtot es rontasz a szobaba",
+        gomb1_hova: 4,
+        gomb2_szoveg: "Inkab visszamesz az elagazashoz",
+        gomb2_hova: 1
+    }
+};
+
+// a jatekos adatai
+let jatekosAdatok = {
+    eletero: 20,
+    szerencse: 9,
+    aktualisFejezet: 1
+};
