86efd4a4b4a47139b39dcdd5b32a3fbec3a2fc66
kujbusadamattila120 <adamka20080920@gmail.com>
2026-05-25 14:25:00 +0200

    ellensegek hozzaadasa

--- app.js
+++ app.js
@@ -1,106 +1,174 @@
-// fejezetek
-let storyNodes = {
-    1: {
-        text: "Gyalogszerrel indulsz utnak. A Tuzhegy labanal egy sotet barlangbejaratot talsz. Belepsz a jaratba. Elotted a folyosó egyenesen halad, de par meter utan egy elagazashoz ersz.",
-        gomb1_szoveg: "Fordulj Nyugat fele",
-        gomb1_hova: 2,
-        gomb2_szoveg: "Fordulj Kelet fele",
-        gomb2_hova: 3
-    },
-    2: {
-        text: "A nyugati jarat egyre szukebb lesz, vegul egy nehez tolgyfa ajtohoz ersz. Halk horkolast hallasz odabentrol.",
-        gomb1_szoveg: "Berugod az ajtot es rontasz a szobaba",
-        gomb1_hova: 4,
-        gomb2_szoveg: "Inkab visszamesz az elagazashoz",
-        gomb2_hova: 1
-    },
-    3: {
-        text: "Kelet felé indulsz, de hirtelen megremeg a fold a labad alatt! Egy csapoajto nyilik meg alattad. Mit teszel?",
-        gomb1_szoveg: "Megprobalok elugrani!",
-        gomb1_hova: 5,
-        gomb2_szoveg: "Visszarohanok a bejarathoz",
-        gomb2_hova: 1
-    },
-    4: {
-        text: "Belerugsz az ajtoba. Egy duhos barlangi Ork ugrik fel az agyarol, fegyvert rantva! (Sajnos itt most veres vegso harc var rad...)",
-        gomb1_szoveg: "Ujra a bejarathoz",
-        gomb1_hova: 1,
-        gomb2_szoveg: "Vissza az ajtohoz",
-        gomb2_hova: 2
-    },
-    5: {
-        text: "Hatalmas reflexszel sikerult elugranod a megnyilo csapda elol! Biztonsagban vagy a folyoso masik oldalan.",
-        gomb1_szoveg: "Tovabb a barlang melyere",
-        gomb1_hova: 6,
-        gomb2_szoveg: " Inkabb megis visszaterok",
-        gomb2_hova: 1
-    },
-    6: {
-        text: "Egy feszitoen csendes kincstarba ersz. Megtalaltad a Tuzhegy Varazslojának elfeledett ladait! Megnyerted a jatekot!",
-        gomb1_szoveg: "Jatek ujrainditasa",
-        gomb1_hova: 1,
-        gomb2_szoveg: "Vissza a csapdahoz",
-        gomb2_hova: 5
+const storyNodes = {
+  1: {
+    text: "A barlang bejaratanal allsz.",
+    image: "images/cave.jpg",
+    choices: [
+      {
+        text: "Bemesz a barlangba",
+        next: 2
+      },
+      {
+        text: "Hazamesz",
+        next: 3
+      }
+    ]
+  },
+
+  2: {
+    text: "Egy ork jelenik meg elotted.",
+    image: "images/ork.jpg",
+    enemy: true,
+    choices: [
+      {
+        text: "Tamadas",
+        next: 4
+      },
+      {
+        text: "Menekules",
+        next: 1
+      }
+    ]
+  },
+
+  3: {
+    text: "Hazamentel. Vege a jateknak.",
+    image: "images/end.jpg",
+    choices: [
+      {
+        text: "Ujrakezdes",
+        next: 1
+      }
+    ]
+  },
+
+  4: {
+    text: "Legyozted az orkot es talaltal egy kardot.",
+    image: "images/treasure.jpg",
+    item: "Rozsdas Kard",
+    choices: [
+      {
+        text: "Tovabb",
+        next: 5
+      }
+    ]
+  },
+
+  5: {
+    text: "Megtalaltad a kincset. Gyoztel!",
+    image: "images/win.jpg",
+    choices: [
+      {
+        text: "Uj jatek",
+        next: 1
+      }
+    ]
+  }
+};
+
+let player = {
+  hp: 20,
+  luck: 9,
+  inventory: [],
+  currentNode: 1
+};
+
+const storyText = document.getElementById("story-text");
+const choicesContainer = document.getElementById("choices-container");
+const hpText = document.getElementById("hp");
+const luckText = document.getElementById("luck");
+const inventoryText = document.getElementById("inventory");
+const chapterImage = document.getElementById("chapter-image");
+
+function saveGame() {
+  localStorage.setItem("kjk_save", JSON.stringify(player));
+}
+
+function loadGame() {
+  let save = localStorage.getItem("kjk_save");
+
+  if (save) {
+    player = JSON.parse(save);
+  }
+}
+
+function updateGame() {
+  let current = storyNodes[player.currentNode];
+
+  storyText.innerText = current.text;
+
+  chapterImage.src = current.image;
+
+  hpText.innerText = player.hp;
+  luckText.innerText = player.luck;
+
+  if (player.inventory.length === 0) {
+    inventoryText.innerText = "Ures";
+  } else {
+    inventoryText.innerText = player.inventory.join(", ");
+  }
+
+  choicesContainer.innerHTML = "";
+
+  if (current.item) {
+    if (!player.inventory.includes(current.item)) {
+      player.inventory.push(current.item);
     }
-};
+  }
 
-// jatekos adatai
-let jatekosAdatok = {
-    eletero: 20,
-    szerencse: 9,
-    aktualisFejezet: 1
-};
+  current.choices.forEach(choice => {
+    let button = document.createElement("button");
 
+    button.innerText = choice.text;
 
-let szovegDoboz = document.getElementById('story-text');
-let gombokDoboz = document.getElementById('choices-container');
-let eleteroKijelzo = document.getElementById('hp');
+    button.onclick = function () {
 
+      if (current.enemy && choice.text === "Tamadas") {
 
-function oldalFrissites() {
-    let aktualisNode = storyNodes[jatekosAdatok.aktualisFejezet];
+        let enemyPower = Math.floor(Math.random() * 6) + 1;
+        let playerPower = Math.floor(Math.random() * 6) + 3;
 
-    szovegDoboz.innerText = aktualisNode.text;
-    eleteroKijelzo.innerText = jatekosAdatok.eletero;
+        if (playerPower >= enemyPower) {
+          alert("Legyozted az orkot!");
+        } else {
+          player.hp -= 5;
 
-    gombokDoboz.innerHTML = '';
-    
-    let gomb1 = document.createElement('button');
-    gomb1.innerText = aktualisNode.gomb1_szoveg;
-    gomb1.onclick = function() {
-        jatekosAdatok.aktualisFejezet = aktualisNode.gomb1_hova;
-        oldalFrissites(); 
+          alert("Megsebesultel!");
+
+          if (player.hp <= 0) {
+            alert("Meghaltal!");
+
+            restartGame();
+            return;
+          }
+        }
+      }
+
+      player.currentNode = choice.next;
+
+      saveGame();
+
+      updateGame();
     };
-    gombokDoboz.appendChild(gomb1);
 
-    let gomb2 = document.createElement('button');
-    gomb2.innerText = aktualisNode.gomb2_szoveg;
-    gomb2.onclick = function() {
-        jatekosAdatok.aktualisFejezet = aktualisNode.gomb2_hova;
-        oldalFrissites();
-    };
-    gombokDoboz.appendChild(gomb2);
-    localStorage.setItem('kjk_mentes', JSON.stringify(jatekosAdatok));
+    choicesContainer.appendChild(button);
+  });
 }
 
-//mentes
-let mentes = localStorage.getItem('kjk_mentes');
-if (mentes) {
-    jatekosAdatok = JSON.parse(mentes);
+function restartGame() {
+  localStorage.removeItem("kjk_save");
+
+  player = {
+    hp: 20,
+    luck: 9,
+    inventory: [],
+    currentNode: 1
+  };
+
+  updateGame();
 }
 
+document.getElementById("restart-btn").onclick = restartGame;
 
-oldalFrissites();
+loadGame();
 
-
-//ujrainditas
-document.getElementById('restart-btn').onclick = function() {
- 
-    localStorage.removeItem('kjk_mentes');
-
-    jatekosAdatok.eletero = 20;
-    jatekosAdatok.szerencse = 9;
-    jatekosAdatok.aktualisFejezet = 1;
-
-    oldalFrissites();
-};
+updateGame();
